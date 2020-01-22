1484ca32851ce98209dccd65da98792d2b646028
Sergio <smg158@alu.ua.es>
2020-01-22 19:35:37 +0100

    Terminados todos los ejercicios 3 de Laravel

--- videoclub/resources/views/catalog/edit.blade.php
+++ videoclub/resources/views/catalog/edit.blade.php
@@ -19,30 +19,30 @@
 
                     <div class="form-group">
                         <label for="title">Título</label>
-                        <input type="text" name="title" id="title" class="form-control">
+                        <input type="text" name="title" id="title" class="form-control" placeholder="{{$pelicula->title}}">
                     </div>
 
                     <div class="form-group">
                         {{-- TODO: Completa el input para el año --}}
                         <label for="year">Año</label>
-                        <input type="text" name="year" id="year" class="form-control">
+                        <input type="text" name="year" id="year" class="form-control" placeholder="{{$pelicula->year}}">
                     </div>
 
                     <div class="form-group">
                         {{-- TODO: Completa el input para el director --}}
                         <label for="director">Director</label>
-                        <input type="text" name="director" id="director" class="form-control">
+                        <input type="text" name="director" id="director" class="form-control" placeholder="{{$pelicula->director}}">
                     </div>
 
                     <div class="form-group">
                         {{-- TODO: Completa el input para el poster --}}
-                        <label for="poster">Título</label>
-                        <input type="text" name="poster" id="poster" class="form-control">
+                        <label for="poster">Imágen</label>
+                        <input type="text" name="poster" id="poster" class="form-control" placeholder="{{$pelicula->poster}}">
                     </div>
 
                     <div class="form-group">
                         <label for="synopsis">Resumen</label>
-                        <textarea name="synopsis" id="synopsis" class="form-control" rows="3"></textarea>
+                        <textarea name="synopsis" id="synopsis" class="form-control" rows="3" placeholder="{{$pelicula->synopsis}}"></textarea>
                     </div>
 
                     <div class="form-group text-center">
--- videoclub/resources/views/catalog/show.blade.php
+++ videoclub/resources/views/catalog/show.blade.php
@@ -7,29 +7,29 @@
     <div class="col-sm-4">
 
         {{-- TODO: Imagen de la película --}}
-        <img src="{{$pelicula['poster']}}" class="float-right" />
+        <img src="{{$pelicula->poster}}" class="float-right" />
     </div>
     <div class="col-sm-8">
 
         {{-- TODO: Datos de la película --}}
-        <h2>{{$pelicula['title']}}</h2>
-        <p><b>Año: </b>{{$pelicula['year']}}<br>
-            <b>Director: </b>{{$pelicula['director']}}<br>
-            <b>Sinopsis: </b>{{$pelicula['synopsis']}}<br>
-
-            @if($pelicula['rented'])
+        <h2>{{$pelicula->title}}</h2>
+        <p><b>Año: </b>{{$pelicula->year}}<br>
+            <b>Director: </b>{{$pelicula->director}}<br><br>
+            <b>Sinopsis: </b>{{$pelicula->synopsis}}<br>
+            <br>
+            @if($pelicula->rented)
             <b>Estado: </b>Película actualmente alquilada
             @else
             <b>Estado: </b>Película disponible para alquilar
             @endif
         </p>
         <p>
-            @if($pelicula['rented'])
+            @if($pelicula->rented)
             <button type="button" class="btn btn-danger">Devolver película</button>
             @else
             <button type="button" class="btn btn-primary">Alquilar película</button>
             @endif
-            <a href="{{action('CatalogController@getEdit', [1])}}" class="btn btn-warning"><i class="fas fa-pen"></i> Editar película</a>
+            <a href="{{action('CatalogController@getEdit', $pelicula->id)}}" class="btn btn-warning"><i class="fas fa-pen"></i> Editar película</a>
             <a href="{{ action('CatalogController@getIndex') }}" class="btn btn-light"><i class="fas fa-chevron-left"></i> Volver al listado</a>
         </p>
 
